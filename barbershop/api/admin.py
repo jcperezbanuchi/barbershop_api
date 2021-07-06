1e17a1a4c6e0da3adeb1f6837c5a1aab2f3cdde4
Juanco <juanco@Juans-Air>
2021-07-06 11:23:38 -0400

    cors added

--- barbershop/api/admin.py
+++ barbershop/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Appointment
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'barber', 'phone', 'email', 'contact', 'date', 'time', 'comment')
+
+admin.site.register(Appointment, AppointmentAdmin)    
--- barbershop/barbershop/settings.py
+++ barbershop/barbershop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'api'
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'barbershop.urls'
@@ -128,3 +131,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
