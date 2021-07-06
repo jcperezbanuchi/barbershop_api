b68bb7b0c555d46c14a530f3050513c6f8423a66
Juanco <juanco@Juans-Air>
2021-07-06 17:50:55 -0400

    fixed venv folder

--- barbershop/barbershop/settings.py
+++ barbershop/barbershop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'api'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,8 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
+
+CORS_ALLOW_ALL_ORIGINS=True
 
 ROOT_URLCONF = 'barbershop.urls'
 
--- barbershop/barbershop/urls.py
+++ barbershop/barbershop/urls.py
@@ -19,7 +19,7 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'appointment', views.AppointmentView, 'api')
+router.register(r'appointments', views.AppointmentView , 'appointment')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
